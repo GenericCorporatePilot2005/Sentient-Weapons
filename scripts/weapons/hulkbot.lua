dbfdcd21767c09193ef55d6c642bc9e5d1ea46b8
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-05-25 22:50:37 +1000

    created weapon for frozen hulk

---
+++ scripts/weapons/hulkbot.lua
@@ -0,0 +1,116 @@
+------Frozen Hulks------
+Nico_hulkbot=ArtilleryDefault:new{
+	Name="Flambeaux Artillery",
+	Class = "TechnoVek",
+	Icon = "weapons/ranged_ignite.png",
+	Description="Launch a Vulcan Artillery shot in one direction, and then a Cluster Artillery shot in a different direction. Freeze Self.",-- Every time this weapon is selected, swap the order of the shots.",
+	Damage = 1,
+	PowerCost = 0,
+	BuildingDamage = false,
+	shield=false,
+	TwoClick = true,
+	Cancel = false,
+	Upgrades = 2,
+	UpgradeList = { "Cancel",  "+1 Damage"  },
+	UpgradeCost = {1,3},
+	LaunchSound = "",
+	ImpactSound = "",
+	TipImage = {
+		Unit = Point(1,3),
+		Building1 = Point(1,2),
+		Building2 = Point(2,3),
+		Target = Point(3,3),
+		Enemy1 = Point(3,3),
+		Enemy2 = Point(2,1),
+		Enemy3 = Point(3,2),
+		Queued1 = Point(2,3),
+		Second_Click = Point(1,1),
+        CustomPawn="Nico_artillerybot_mech",
+	},
+	}
+	function Nico_hulkbot:GetSecondTargetArea(p1, p2)  --This is a copy of the GetTargetArea for LineArtillery
+		local ret = PointList()
+		local dir = GetDirection(p2 - p1)
+		for j = 1, 3 do
+			for i = 2, 8 do
+				local curr = Point(p1 + DIR_VECTORS[(dir+j)%4] * i)
+				if not Board:IsValid(Point(p1 + DIR_VECTORS[(dir+j)%4] * i)) then  
+					break
+				end
+				ret:push_back(curr)
+			end
+		end
+		return ret
+	end
+	function Nico_hulkbot:GetSkillEffect(p1,p2)
+		local ret = SkillEffect()
+		local direction = GetDirection(p1 - p2)
+		
+		local ice = SpaceDamage(p1,0)
+		ice.iFrozen = 1
+		ret:AddDamage(ice)
+		ret:AddSound("/weapons/fireball")
+		ret:AddBounce(p1, 1)
+		
+		local damage = SpaceDamage(p2,0)
+		damage.sAnimation = "ExploArt2"
+		damage.iFire = 1
+		ret:AddArtillery(damage, "effects/shotup_ignite_fireball.png")
+		
+		for dir = DIR_START, DIR_END do
+			damage = SpaceDamage(p2 + DIR_VECTORS[dir], 0)
+			damage.iPush = dir
+			damage.sAnimation = "airpush_"..dir
+			ret:AddDamage(damage)
+		end
+		ret:AddBounce(p2, self.BounceAmount)
+		ret:AddSound("/props/fire_damage")
+		if self.Cancel and Board:IsPawnSpace(p2) and Board:IsPawnTeam(p2, TEAM_ENEMY) then
+			if Board:GetPawn(p2):IsQueued() or Board:IsTipImage() then
+				ret:AddScript("Board:AddAlert("..p2:GetString()..",\"ATTACK CANCELED\")")
+				ret:AddScript("Board:GetPawn("..p2:GetString().."):ClearQueued()")
+			end
+			local web_id = Board:GetPawn(p2):GetId()--Store pawn id
+			ret:AddScript("Board:GetPawn("..web_id.."):SetSpace(Point(-1,-1))")--Move the pawn to Point(-1,-1) to delete webbing
+			local damage = SpaceDamage(p1,0)
+			damage.bHide = true
+			damage.fDelay = 0.00017--force a one frame delay on the board
+			ret:AddDamage(damage)
+			ret:AddScript("Board:GetPawn("..web_id.."):SetSpace("..p2:GetString()..")")--Move the pawn back
+			return ret
+		end
+		ret:AddDelay(FULL_DELAY)
+		return ret
+	end
+	function Nico_hulkbot:GetFinalEffect(p1,p2,p3)
+		local ret = self:GetSkillEffect(p1,p2)
+		local dir = GetDirection(p3 - p1)
+		
+		local temp = Ranged_Defensestrike_A:GetSkillEffect(p1,p3)
+		if self.Damage == 2 then temp = Ranged_Defensestrike_AB:GetSkillEffect(p1,p3) end
+		
+		ret:AddSound("/weapons/defense_strike")
+		for i = 1, temp.effect:size() do
+			ret:AddDamage(temp.effect:index(i))
+		end
+		ret:AddSound("/impact/generic/explosion")
+		return ret
+	end
+	Nico_hulkbot_A=Nico_hulkbot:new{
+		UpgradeDescription = "The Vulcan Artillery shot cancels the target unit on impact.",
+		Cancel = true,
+	}
+	Nico_hulkbot_B=Nico_hulkbot:new{
+		Damage=2,
+		UpgradeDescription = "Increases damage of the Cluster Artillery shot by 1.",
+	}
+	Nico_hulkbot_AB=Nico_hulkbot_A:new{
+		Damage=2,
+	}
+	modApi:addWeaponDrop("Nico_hulkbot")
+	
+	--local function EVENT_onModsLoaded() --This function will run when the mod is loaded
+	--hulkbot_counter = false
+	--end
+
+	--modApi.events.onModsLoaded:subscribe(EVENT_onModsLoaded)
