ddd8141209924a40283020162774d3f70957cff1
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-05-30 09:29:04 +1000

    just quickly flip the chasm to ground and then revert it. but please test this.

--- scripts/init.lua
+++ scripts/init.lua
@@ -73,8 +73,9 @@ function mod:load( options, version)
                     LOG(pawn:GetMechName() .." is dead")
                     
                     if pawn:IsAbility("Nico_BotRepair") then
+						spot = pawn.loc
 						if pawn.loc==TERRAIN_HOLE then
-							pawn:SetHealth(1) Board:RemovePawn(pawn) Board:AddPawn(pawn, Point(0,7)) pawn:SetSpace(Point(-1,-1))
+							pawn:SetHealth(1) Board:RemovePawn(pawn) Board:SetTerrain(spot,0) Board:AddPawn(pawn,spot) pawn:SetSpace(Point(-1,-1)) Board:SetTerrain(spot,9)
 							LOG("reviving Leader Bot")
 						else
 							pawn:SetHealth(1)
