538c51d07bb7718deacb5c568e72384b3e2d11f5
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-05-30 10:58:33 +1000

    fixed clashing variable naming

--- scripts/weapons/otherweapons.lua
+++ scripts/weapons/otherweapons.lua
@@ -13,16 +13,16 @@ local function Nico_MoveShieldWeapon(mission, pawn, weaponId, p1, p2, p3, skillE
 		local spaceDamage = skillEffect.effect:index(i)
 		if spaceDamage:IsMovement() and spaceDamage:GetMoveType() == 0 then
 			local movePawn = Board:GetPawn(spaceDamage:MoveStart())
-			local i = movePawn:GetId()
+			local j = movePawn:GetId()
 			local IsRealMission = true and (mission ~= nil) and (mission ~= Mission_Test) and Board	and Board:IsMissionBoard()
 			local endpoint = spaceDamage:MoveEnd()--spaceDamage:MoveStart()
-			local adjacent_mech = IsRealMission and ((Board:GetPawn((i+1)%3):GetSpace():Manhattan(endpoint)==1) or (Board:GetPawn((i+2)%3):GetSpace():Manhattan(endpoint)==1))
+			local adjacent_mech = IsRealMission and ((Board:GetPawn((j+1)%3):GetSpace():Manhattan(endpoint)==1) or (Board:GetPawn((j+2)%3):GetSpace():Manhattan(endpoint)==1))
 			if movePawn and _G[movePawn:GetType()].NicoIsRobot and adjacent_mech then
 				local dam = SpaceDamage(spaceDamage:MoveStart(),0)
 				dam.iShield = 1
 				skillEffect:AddDamage(dam)
 				skillEffect:AddScript("Game:TriggerSound(\"/props/shield_activated\")")
-				skillEffect:AddScript("Board:GetPawn("..i.."):SetShield(true)")
+				skillEffect:AddScript("Board:GetPawn("..j.."):SetShield(true)")
 			end
 		end
 	end
