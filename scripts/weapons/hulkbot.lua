46509396043a7ed78d35bad31030f6bdbb18f6af
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-05-31 11:46:06 +1000

    swapped order of shots as requested

--- scripts/weapons/hulkbot.lua
+++ scripts/weapons/hulkbot.lua
@@ -3,7 +3,7 @@ Nico_hulkbot=ArtilleryDefault:new{
 	Name="Artillery Mark III",
 	Class = "TechnoVek",
 	Icon = "weapons/ranged_ignite.png",
-	Description="Launch a Vulcan Artillery shot in one direction, and then a Cluster Artillery shot in a different direction. Freeze Self.",-- Every time this weapon is selected, swap the order of the shots.",
+	Description="Launch a Cluster Artillery shot in one direction, and then a Vulcan Artillery shot in a different direction. Freeze Self.",-- Every time this weapon is selected, swap the order of the shots.",
 	Damage = 1,
 	PowerCost = 0,
 	BuildingDamage = false,
@@ -19,12 +19,12 @@ Nico_hulkbot=ArtilleryDefault:new{
 		Unit = Point(1,3),
 		Building1 = Point(1,2),
 		Building2 = Point(2,3),
-		Target = Point(3,3),
+		Target = Point(1,1),
 		Enemy1 = Point(3,3),
 		Enemy2 = Point(2,1),
 		Enemy3 = Point(3,2),
 		Queued1 = Point(2,3),
-		Second_Click = Point(1,1),
+		Second_Click = Point(3,3),
         CustomPawn="Nico_hulkbot_mech",
 	},
 	}
@@ -44,56 +44,48 @@ Nico_hulkbot=ArtilleryDefault:new{
 	end
 	function Nico_hulkbot:GetSkillEffect(p1,p2)
 		local ret = SkillEffect()
-		local direction = GetDirection(p1 - p2)
-		
-		local ice = SpaceDamage(p1,0)
-		ice.iFrozen = 1
-		ret:AddDamage(ice)
-		ret:AddSound("/weapons/fireball")
-		ret:AddBounce(p1, 1)
-		
-		local damage = SpaceDamage(p2,0)
-		damage.sAnimation = "ExploArt2"
-		damage.iFire = 1
-		ret:AddArtillery(damage, "effects/shotup_ignite_fireball.png")
-		
-		for dir = DIR_START, DIR_END do
-			damage = SpaceDamage(p2 + DIR_VECTORS[dir], 0)
-			damage.iPush = dir
-			damage.sAnimation = "airpush_"..dir
-			ret:AddDamage(damage)
-		end
-		ret:AddBounce(p2, self.BounceAmount)
-		ret:AddSound("/props/fire_damage")
-		if self.Cancel and Board:IsPawnSpace(p2) and Board:IsPawnTeam(p2, TEAM_ENEMY) then
-			if Board:GetPawn(p2):IsQueued() or Board:IsTipImage() then
-				ret:AddScript("Board:AddAlert("..p2:GetString()..",\"ATTACK CANCELED\")")
-				ret:AddScript("Board:GetPawn("..p2:GetString().."):ClearQueued()")
-			end
-			local web_id = Board:GetPawn(p2):GetId()--Store pawn id
-			if Board:GetPawn(p2):GetType() ~= "tosx_IceHornet" then ret:AddScript("Board:GetPawn("..web_id.."):SetSpace(Point(-1,-1))") end--Move the pawn to Point(-1,-1) to delete webbing
-			local damage = SpaceDamage(p1,0)
-			damage.bHide = true
-			damage.fDelay = 0.00017--force a one frame delay on the board
-			ret:AddDamage(damage)
-			ret:AddScript("Board:GetPawn("..web_id.."):SetSpace("..p2:GetString()..")")--Move the pawn back
-			return ret
-		end
-		ret:AddDelay(FULL_DELAY)
-		return ret
-	end
-	function Nico_hulkbot:GetFinalEffect(p1,p2,p3)
-		local ret = self:GetSkillEffect(p1,p2)
-		local dir = GetDirection(p3 - p1)
-		
-		local temp = Ranged_Defensestrike_A:GetSkillEffect(p1,p3)
-		if self.Damage == 2 then temp = Ranged_Defensestrike_AB:GetSkillEffect(p1,p3) end
+		local temp = Ranged_Defensestrike_A:GetSkillEffect(p1,p2)
+		if self.Damage == 2 then temp = Ranged_Defensestrike_AB:GetSkillEffect(p1,p2) end
 		
 		ret:AddSound("/weapons/defense_strike")
 		for i = 1, temp.effect:size() do
 			ret:AddDamage(temp.effect:index(i))
 		end
 		ret:AddSound("/impact/generic/explosion")
+		
+		ret:AddDelay(FULL_DELAY)
+		return ret
+	end
+	function Nico_hulkbot:GetFinalEffect(p1,p2,p3)
+		local ret = self:GetSkillEffect(p1,p2)
+		local ice = SpaceDamage(p1,0)
+		ice.iFrozen = 1
+		ret:AddDamage(ice)
+		ret:AddSound("/weapons/fireball")
+		ret:AddBounce(p1, 1)
+		
+		local damage = SpaceDamage(p3,0)
+		damage.sAnimation = "ExploArt2"
+		damage.iFire = 1
+		ret:AddArtillery(damage, "effects/shotup_ignite_fireball.png")
+		
+		for dir = DIR_START, DIR_END do
+			damage = SpaceDamage(p3 + DIR_VECTORS[dir], 0)
+			damage.iPush = dir
+			damage.sAnimation = "airpush_"..dir
+			ret:AddDamage(damage)
+		end
+		ret:AddBounce(p3, self.BounceAmount)
+		if self.Cancel then
+			if Board:IsTipImage() then
+				ret:AddScript("Board:AddAlert("..p3:GetString()..",\"ATTACK CANCELED\")")
+				ret:AddScript("Board:GetPawn("..p3:GetString().."):ClearQueued()")
+			else
+				ret:AddScript("modApi:runLater(function() Board:SetSmoke("..p3:GetString()..",true,true) modApi:runLater(function() Board:SetFire("..p3:GetString()..",true) end) end)")
+			end
+		else
+			ret:AddSound("/props/fire_damage")
+		end
 		return ret
 	end
 	Nico_hulkbot_A=Nico_hulkbot:new{
@@ -114,4 +106,3 @@ Nico_hulkbot=ArtilleryDefault:new{
 	--end
 
 	--modApi.events.onModsLoaded:subscribe(EVENT_onModsLoaded)
-	
