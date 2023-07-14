c9e3147c898ea08820a112ce46dcac2f82603857
WolframParadoxica <127800491+WolframParadoxica@users.noreply.github.com>
2023-07-14 17:41:54 +1000

    knight/jugger changes (affects any weapon, with any movement type)

--- scripts/weapons/otherweapons.lua
+++ scripts/weapons/otherweapons.lua
@@ -8,11 +8,32 @@ local function Nico_MoveShield(mission, pawn, weaponId, p1, p2)
 	end
 end
 
+local function Nico_MoveShieldWeap(mission, pawn, weaponId, p1, p2, skillEffect)
+	if weaponId == "Nico_minibot" or weaponId == "Nico_minibot2" or weaponId == "Move" then return end
+	for i = 1, skillEffect.effect:size() do
+		local spaceDamage = skillEffect.effect:index(i)
+		if spaceDamage:IsMovement() and spaceDamage:GetMoveType() ~= 3 then
+			local movePawn = pawn
+			local j = movePawn:GetId()
+			local IsRealMission = true and (mission ~= nil) and (mission ~= Mission_Test) and Board	and Board:IsMissionBoard()
+			local endpoint = spaceDamage:MoveEnd()--spaceDamage:MoveStart()
+			local adjacent_mech = IsRealMission and ((Board:GetPawn((j+1)%3):GetSpace():Manhattan(endpoint)==1) or (Board:GetPawn((j+2)%3):GetSpace():Manhattan(endpoint)==1))
+			if movePawn and _G[movePawn:GetType()].NicoIsRobot and adjacent_mech then
+				local dam = SpaceDamage(spaceDamage:MoveStart(),0)
+				dam.sImageMark = "combat/icons/icon_shield_glow.png"
+				skillEffect:AddDamage(dam)
+				skillEffect:AddScript("Game:TriggerSound(\"/props/shield_activated\")")
+				skillEffect:AddScript("Board:GetPawn("..j.."):SetShield(true)")
+			end
+		end
+	end
+end
+
 local function Nico_MoveShieldWeapon(mission, pawn, weaponId, p1, p2, p3, skillEffect)
 	if weaponId == "Nico_minibot" or weaponId == "Nico_minibot2" then return end
 	for i = 1, skillEffect.effect:size() do
 		local spaceDamage = skillEffect.effect:index(i)
-		if spaceDamage:IsMovement() and spaceDamage:GetMoveType() == 0 then
+		if spaceDamage:IsMovement() and spaceDamage:GetMoveType() ~= 3 then
 			local movePawn = Board:GetPawn(spaceDamage:MoveStart())
 			local j = movePawn:GetId()
 			local IsRealMission = true and (mission ~= nil) and (mission ~= Mission_Test) and Board	and Board:IsMissionBoard()
@@ -20,7 +41,7 @@ local function Nico_MoveShieldWeapon(mission, pawn, weaponId, p1, p2, p3, skillE
 			local adjacent_mech = IsRealMission and ((Board:GetPawn((j+1)%3):GetSpace():Manhattan(endpoint)==1) or (Board:GetPawn((j+2)%3):GetSpace():Manhattan(endpoint)==1))
 			if movePawn and _G[movePawn:GetType()].NicoIsRobot and adjacent_mech then
 				local dam = SpaceDamage(spaceDamage:MoveStart(),0)
-				dam.iShield = 1
+				dam.sImageMark = "combat/icons/icon_shield_glow.png"
 				skillEffect:AddDamage(dam)
 				skillEffect:AddScript("Game:TriggerSound(\"/props/shield_activated\")")
 				skillEffect:AddScript("Board:GetPawn("..j.."):SetShield(true)")
@@ -72,6 +93,7 @@ end
 
 local function EVENT_onModsLoaded()
 	modapiext:addSkillStartHook(Nico_MoveShield)
+	modapiext:addSkillBuildHook(Nico_MoveShieldWeap)
 	modapiext:addFinalEffectBuildHook(Nico_MoveShieldWeapon)
 	modapiext:addTargetAreaBuildHook(Nico_TeamRepair)
 	modApi:addMissionUpdateHook(Nico_BoomTest)
